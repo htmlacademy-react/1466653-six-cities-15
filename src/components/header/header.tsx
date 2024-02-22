cd9b0c187f70ed4b097e7af782a9d7f520b2dad1
Goulshat <76894691+Goulshat@users.noreply.github.com>
2024-02-23 03:11:23 +0500

    added links in Header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Header: FC = () => (
   <header className="header">
@@ -18,21 +19,21 @@ export const Header: FC = () => (
         <nav className="header__nav">
           <ul className="header__nav-list">
             <li className="header__nav-item user">
-              <a
+              <Link
                 className="header__nav-link header__nav-link--profile"
-                href="#"
+                to="/favorites"
               >
                 <div className="header__avatar-wrapper user__avatar-wrapper"></div>
                 <span className="header__user-name user__name">
                   Oliver.conner@gmail.com
                 </span>
                 <span className="header__favorite-count">3</span>
-              </a>
+              </Link>
             </li>
             <li className="header__nav-item">
-              <a className="header__nav-link" href="#">
+              <Link className="header__nav-link" to="#">
                 <span className="header__signout">Sign out</span>
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
