eccfc3c595518a7a02d9600953c9c4a19ae11478
Goulshat <76894691+Goulshat@users.noreply.github.com>
2024-02-22 22:57:49 +0500

    added Router into project

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,25 @@
 import { FC, PropsWithChildren } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { MainPage } from './../pages/main/';
+import { LoginPage } from '../pages/login';
+import { FavoritesPage } from '../pages/favorites';
+import { OfferPage } from '../pages/offer';
+import { NotFoundPage } from '../pages/not-found';
 
-export type TAppProps = { cardAmount: number };
+export type TAppProps = { cardAmount?: number };
 
 export const App: FC<PropsWithChildren<TAppProps>> = ({ cardAmount }) => (
-  <MainPage cardAmount={ cardAmount } />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<MainPage cardAmount={ cardAmount } />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+        <Route path="offer" element={<OfferPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
