807cdef7413e25ed55936f1ad111e3b0fb32deb7
Goulshat <76894691+Goulshat@users.noreply.github.com>
2024-03-11 11:15:17 +0500

    added 'activeCity' state into main-page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Nullable } from 'vitest';
 import { TAppProps } from '../../app';
 import { TCity } from '../../types/offer';
 import { TabList } from '../../components/tab-list';
@@ -9,7 +10,11 @@ import { OffersEmpty } from '../../components/offers-empty';
 type TMainPageProps = Pick<TAppProps, 'offers' | 'cities'>;
 
 export const MainPage: FC<TMainPageProps> = ({ offers, cities }) => {
-  const [activeCity, setActiveCity] = useState<TCity>(cities[0]);
+  const [activeCity, setActiveCity] = useState<Nullable<TCity>>(null);
+
+  const selectCityHandler = (city: Nullable<TCity>) => {
+    setActiveCity(city);
+  };
 
   return (
     <main className={`page__main page__main--index ${offers.length === 0 ?? 'page__main--index-empty'}`}>
@@ -19,7 +24,7 @@ export const MainPage: FC<TMainPageProps> = ({ offers, cities }) => {
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <section className="locations container">
-          <TabList />
+          <TabList cities={ cities } selectHandler={ selectCityHandler } />
         </section>
       </div>
       <div className="cities">
