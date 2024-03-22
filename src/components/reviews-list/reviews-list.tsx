dec543258ee7924ff7ef337664451341717789e2
Goulshat <76894691+Goulshat@users.noreply.github.com>
2024-03-22 19:24:16 +0500

    refactored ReviewsList and OffersPage components, created ReviewsItem component

--- src/components/reviews-list/reviews-list.tsx
+++ src/components/reviews-list/reviews-list.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Setting } from '../../const';
 import DateHelper from '../../helpers/date';
-import { TComment } from '../../types/comment';
+import { ReviewsItem } from '../reviews-item';
 
 type TReviewsListProps = {
   comments: TComment[];
@@ -19,47 +19,21 @@ export const ReviewsList: FC<TReviewsListProps> = ({comments}) => {
     .sort((a, b) => (b.date.getTime() - a.date.getTime()));
 
   return (
-    <ul className="reviews__list">
-      {
-        renderedComments.map(({ user, comment, rating, date }) => {
-          const ratingWidthStyle = `${rating * (100 / Setting.MAX_RATING)}%`;
+    <section className="offer__reviews reviews">
+      <h2 className="reviews__title">
+        Reviews · <span className="reviews__amount">{comments.length}</span>
+      </h2>
 
-          return (
-            <li
-              className="reviews__item"
-              // eslint-disable-next-line react/no-array-index-key
-              key={`${user.name}_${date.getTime()}`}
-            >
-              <div className="reviews__user user">
-                <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                  <img
-                    className="reviews__avatar user__avatar"
-                    src={user.avatarUrl}
-                    width={54}
-                    height={54}
-                    alt="Reviews avatar"
-                  />
-                </div>
-                <span className="reviews__user-name">{user.name}</span>
-              </div>
-              <div className="reviews__info">
-                <div className="reviews__rating rating">
-                  <div className="reviews__stars rating__stars">
-                    <span style={{ width: ratingWidthStyle }} />
-                    <span className="visually-hidden">Rating</span>
-                  </div>
-                </div>
-                <p className="reviews__text">
-                  { comment }
-                </p>
-                <time className="reviews__time" dateTime={ DateHelper.formatDatetime(date) }>
-                  { DateHelper.formatMonthYear(date) }
-                </time>
-              </div>
-            </li>
-          );
-        })
-      }
-    </ul>
+      <ul className="reviews__list">
+        {
+          renderedComments.map((comment) => (
+            <ReviewsItem
+              key={comment.id}
+              comment={comment}
+            />
+          ))
+        }
+      </ul>
+    </section>
   );
 };
